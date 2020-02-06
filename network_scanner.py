230abd7fd6a7bd2c03160cc19a5ba30c7ed71e61
Shaji <kevin.shaji@hrblock.com>
2020-02-06 17:03:41 +0530

    Modified the output using lists

--- network_scanner.py
+++ network_scanner.py
@@ -6,8 +6,13 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    answered = scapy.srp(arp_request_broadcast, timeout=1)[0]
     print(answered.summary())
+    
+    for element in answered:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("----------------------------------------------------")
 
 
 scan("192.168.43.1/24")
