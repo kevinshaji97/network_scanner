e0fad90a215c84f81f2c59e8f0b9ac05270e87bd
Kevin Shaji <ckevin.shaji@gmail.com>
2020-02-10 17:37:49 +0530

    Added argparser to the module

--- network_scanner.py
+++ network_scanner.py
@@ -1,4 +1,12 @@
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
+    return options
 
 
 def scan(ip):
@@ -7,11 +15,21 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
     answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    
-    print("IP \t\t\t MAC_ADDRESS \n --------------------------------------")
-  
+    client_list = [] 
     for element in answered:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_list.append(client_dict)
+    return client_list
 
 
-scan("192.168.43.1/24")
+def print_result(results_list):
+
+    print("IP \t\t\t MAC_ADDRESS \n -------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
